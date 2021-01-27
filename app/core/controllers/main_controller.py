3337c94dfa196f5c8552b3d846a634def431cfe3
SerzhKorochan <serzhkorochan@gmail.com>
2021-01-27 13:47:59 +0200

    CL Interface -- completed

--- app/core/controllers/main_controller.py
+++ app/core/controllers/main_controller.py
@@ -6,8 +6,25 @@ import os
 class MainController:
 
     def __init__(self, config_path):
-
         if os.path.exists(config_path):
             with open(config_path, 'r', encoding='utf-8') as f:
                 self.config = json.load(f)
-            model = CommandLineModel(self.config)
+            self.cl_model = CommandLineModel(self.config)
+
+    def run(self):
+        if self.cl_model.get_entered_arg():
+            entered_arg = self.cl_model.get_entered_arg()
+        else:
+            exit('Input Error!')
+
+        if entered_arg == 'quickstart':
+            pass
+
+        elif entered_arg == 'rm-data':
+            pass
+
+        elif entered_arg == 'create-backup':
+            pass
+
+        elif entered_arg == 'help':
+            pass
--- app/core/models/command_line_model.py
+++ app/core/models/command_line_model.py
@@ -2,6 +2,17 @@ from sys import argv
 
 
 class CommandLineModel:
+    MAX_ARGS_QUANTITY = 2
+
     def __init__(self, config: dict):
         self.config = config
         self.arguments = argv
+
+    def get_entered_arg(self):
+        if len(self.arguments) == self.MAX_ARGS_QUANTITY:
+            entered_arg = self.arguments[1]
+
+            if entered_arg in self.config['available_args']:
+                return entered_arg
+
+        return None
